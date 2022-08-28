c4f66bf5ff59715fda7bbcfeca653b78e8934191
JelicaMitic <mitic.jelica22@gmail.com>
2022-08-28 09:58:15 +0200

    Select meni za izbor dana

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-MySQL-AJAX Bioskop</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+    <h3 id="welcome-heading">Dobro došli u ITECH bioskop</h3>
+
+    <select id="select-dan">
+        <option value="">Izaberi dan</option>
+        <option value="Ponedeljak">Ponedeljak</option>
+        <option value="Utorak">Utorak</option>
+        <option value="Sreda">Sreda</option>
+        <option value="Cetvrtak">Četvrtak</option>
+        <option value="Petak">Petak</option>
+        <option value="Subota">Subota</option>
+        <option value="Nedelja">Nedelja</option>
+    </select>
+
+
+</body>
+
+</html>
